159859e9cb3c6c1ed25e32838626a040b1e58273
Kohányi Róbert <kohanyi.robert@gmail.com>
2024-06-03 20:23:23 +0000

    Fix up.sh and add down.sh

--- scripts/up.sh
+++ scripts/up.sh
@@ -1,7 +1,7 @@
 docker-compose down --volumes --remove-orphans
-rm -rf ./.env
-rm -rf ./docker-compose.yml
-rm -rf -Recurse ./out
+rm -f ./.env
+rm -f ./docker-compose.yml
+rm -rf  ./out
 python ./scripts/generate.py
 docker-compose build
 docker-compose up --detach
