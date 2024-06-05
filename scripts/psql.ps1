995a0c1f54ac508c668faff899b512b417690cf6
Kohányi Róbert <kohanyi.robert@gmail.com>
2024-06-05 23:05:58 +0200

    Replace test:test with admin:admin

--- scripts/psql.ps1
+++ scripts/psql.ps1
@@ -1,1 +1,1 @@
-& docker-compose run --rm utility psql 'host=pgpool user=test password=test dbname=test' -c "$args"
+& docker-compose run --rm utility psql 'host=pgpool user=admin password=admin dbname=admin' -c "$args"
---
+++ scripts/psql.sh
@@ -0,0 +1,1 @@
+docker-compose run --rm utility psql 'host=pgpool user=admin password=admin dbname=admin' -c "$args"
